c7c8956eb3b0d4ecbbffbab07f0f1f6e5b0e3e78
Javier Garcia <fragarco@gmail.com>
2026-01-08 20:04:11 +0100

    build docs

--- docs/build.bat
+++ docs/build.bat
@@ -4,19 +4,30 @@ REM using PANDOC (must be present in the system) and its default HTML template.
 
 SET PANDOC=pandoc --standalone --variable=maxwidth:50em
 
-del .\en\*.html
-del .\es\*.html
+IF "%1"=="clear" (
+    del .\html\en\*.html
+    del .\html\es\*.html
+    rmdir .\html\en
+    rmdir .\html\es
+    rmdir .\html
+) ELSE (
+    if not exist ".\html" mkdir .\html
+    if not exist ".\html\en" mkdir .\html\en
+    if not exist ".\html\es" mkdir .\html\es
 
-%PANDOC% .\en\abasc.md -o .\en\abasc.html
-%PANDOC% .\en\abasm.md -o .\en\abasm.html
-%PANDOC% .\en\cdt.md -o .\en\cdt.html
-%PANDOC% .\en\dsk.md -o .\en\dsk.html
-%PANDOC% .\en\dsk.md -o .\en\img.html
+    %PANDOC% .\en\abasc.md -o  .\html\en\abasc.html
+    %PANDOC% .\en\abasm.md -o  .\html\en\abasm.html
+    %PANDOC% .\en\basprj.md -o .\html\en\basprj.html
+    %PANDOC% .\en\cdt.md -o    .\html\en\cdt.html
+    %PANDOC% .\en\dsk.md -o    .\html\en\dsk.html
+    %PANDOC% .\en\img.md -o    .\html\en\img.html
 
-%PANDOC% .\en\abasc.md -o .\en\abasc.html
-%PANDOC% .\es\abasm.md -o .\es\abasm.html
-%PANDOC% .\es\cdt.md -o .\es\cdt.html
-%PANDOC% .\es\dsk.md -o .\es\dsk.html
-%PANDOC% .\en\dsk.md -o .\es\img.html
+    %PANDOC% .\es\abasc.md -o  .\html\es\abasc.html
+    %PANDOC% .\es\abasm.md -o  .\html\es\abasm.html
+    %PANDOC% .\es\basprj.md -o .\html\es\basprj.html
+    %PANDOC% .\es\cdt.md -o    .\html\es\cdt.html
+    %PANDOC% .\es\dsk.md -o    .\html\es\dsk.html
+    %PANDOC% .\es\img.md -o    .\html\es\img.html
+)
 
 @echo on
