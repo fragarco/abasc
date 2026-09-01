bf6c3bb284ed77f6fada6fc1c96d8548e2bfc357
Javier Garcia <fragarco@gmail.com>
2026-09-01 20:57:42 +0200

    emit an error if the label of AFTER doesn't exist

--- src/emitters/cpcemitter.py
+++ src/emitters/cpcemitter.py
@@ -511,6 +511,8 @@ class CPCEmitter:
             sym = self.symtable.find(str(label.value), SymType.Label, "")
             if sym is not None:
                 self._emit_code(f"ld      de,{sym.label}")
+            else:
+                self._raise_error(38, label)
         self._emit_code(f"call    {FWCALL.KL_INIT_EVENT}", info="KL_INIT_EVENT")
         self._emit_code("pop     hl", info="tick block address")
         self._emit_code("pop     de", info="timer ticks needed to fire the event")
